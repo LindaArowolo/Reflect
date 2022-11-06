6331325e1633bdd5a2346065ca08b8ec607ce740
Linda <linda.arowolo@gmail.com>
2022-11-06 14:37:28 +0000

    test for mock activity

---
+++ testing/test_mock_activity_api.py
@@ -0,0 +1,14 @@
+import unittest
+from api.mock_activity_api import mock_activity_data
+
+
+class TestMockActivityApi(unittest.TestCase):
+
+    def test_activity(self):
+        mock_activity_api = mock_activity_data
+        self.assertIn("Go on a 3-5km walk, try listening to your favourite playlist whilst doing so.", mock_activity_api)
+        self.assertNotIn('Try running a marathon', mock_activity_api)
+
+
+if __name__ == '__main__':
+    unittest.main()
